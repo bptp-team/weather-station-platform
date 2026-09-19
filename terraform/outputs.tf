296f94f4b486abc8048bb297cef63dca2cf5e96d
Thomas Palma <66331874+thomaspalma1@users.noreply.github.com>
2026-09-19 09:51:22 -0300

    [refactor] update terraform output descriptions and remove unused output
    - updated descriptions for resource group, public fqdn, acr name, and acr login server outputs;
    - removed unused vm_identity_principal_id output from terraform configuration.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,5 @@
 output "resource_group_name" {
-  description = "Name of the created resource group."
+  description = "Name of the resource group, used to scope az commands."
   value       = azurerm_resource_group.main.name
 }
 
@@ -8,27 +8,22 @@ output "vm_name" {
   value       = azurerm_linux_virtual_machine.main.name
 }
 
-output "vm_identity_principal_id" {
-  description = "Principal ID of the system assigned identity used to pull images."
-  value       = azurerm_linux_virtual_machine.main.identity[0].principal_id
-}
-
 output "public_ip_address" {
   description = "Public IP address of the VM."
   value       = azurerm_public_ip.main.ip_address
 }
 
 output "public_fqdn" {
-  description = "Public DNS name of the VM. Used by the ESP32 firmware to reach the broker."
+  description = "Public DNS name of the VM. Used by the ESP32 firmware and by server_name in edge/nginx.conf."
   value       = azurerm_public_ip.main.fqdn
 }
 
 output "acr_name" {
-  description = "Name of the Azure Container Registry."
+  description = "Name of the container registry, used by az acr login."
   value       = azurerm_container_registry.main.name
 }
 
 output "acr_login_server" {
-  description = "Registry hostname used in image tags and in the compose file."
+  description = "Registry hostname used in image tags."
   value       = azurerm_container_registry.main.login_server
 }
